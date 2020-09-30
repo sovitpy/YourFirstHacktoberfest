39140fa3f63ba01741a68037c0cdfa12a248fe7c
Binit Ghimire <thebinitghimire@gmail.com>
2020-10-01 01:14:39 +0545

    Problem #1 Added!

---
+++ programming/problem1.c
@@ -0,0 +1,16 @@
+// "Hello, World!" Challenge
+/* 
+	Hello there,
+	
+	I wanted to write a program in C that would print "Hello, world!", but it is instead printing "Hi, world!". Would you mind fixing this problem?
+	
+	The first person to do so in the correct way and create a PR for it would be eligible for his/her PR to be merged.
+	
+	Thanks,
+	Binit Ghimire
+*/
+#include <stdio.h>
+
+int main(){
+	printf("Hi, world!");
+}
