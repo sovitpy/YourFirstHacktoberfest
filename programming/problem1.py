7ab9c93b3485820457154e45a33b99f26f9d940c
Binit Ghimire <thebinitghimire@gmail.com>
2020-10-01 01:22:46 +0545

    New Problem #1 Added!

---
+++ programming/problem1.py
@@ -0,0 +1,12 @@
+# "Hello, World!" Challenge
+"""
+	Hello there,
+	
+	I wanted to write a program in Python 3 that would print "Hello, world!", but it is instead printing "Hi, world!". Would you mind fixing this problem?
+	
+	The first person to do so in the correct way and create a PR for it would be eligible for his/her PR to be merged.
+	
+	Thanks,
+	Binit Ghimire
+"""
+print("Hi, world!")
