39ba475f1f5485a99c0f558eaae49199e2cf32d3
Nachiketa Vadera <programming@sent.at>
2020-10-03 15:39:24 +0530

    finished read from file problem

--- file-print.py
+++ file-print.py
@@ -1,5 +1,7 @@
 # I want to print my lines from a file by python
 # but i can't do that can you do??
 
-a = input("Enter File")
-print(a)
+file_path = input('Enter a file to read: ')
+
+with open(file_path) as file_pointer:
+    print(file_pointer.read())
