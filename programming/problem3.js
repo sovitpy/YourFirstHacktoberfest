28774ea1e72f528e7b7e74dc412a266a2551aafd
Anjal Binayak <anjal@crystalsoft.com.np>
2020-10-18 15:29:15 +0545

    problem3.js solved

--- programming/problem3.js
+++ programming/problem3.js
@@ -6,3 +6,9 @@ Hello there !!
 The first person to do so in the correct way and create a PR for it would be eligible for his/her PR to be merged.	
 
 */
+
+let num1 = parseInt(prompt("Enter the first number"));
+let num2 = parseInt(prompt("Enter the second number"));
+
+
+document.write(num1+num2);
