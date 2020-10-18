1d55f66a96304a795711b70532630fc9d529cec6
AsHok <chapagaiashok@gmail.com>
2020-10-18 10:16:09 +0545

    Added String Concatination Program in Java

--- programming/problem2.java
+++ programming/problem2.java
@@ -1,3 +1,9 @@
 // "String Concatenation!"
 
 // Write a program in Java to concatenate two strings from user input!
+class TestStringConcatenation{  
+ public static void main(String args[]){  
+   String s="String"+" Concatenation";  
+   System.out.println(s);//String Concatenation  
+ }  
+}  
