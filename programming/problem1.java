c0280446f6ff5c82564723c2eded2fab5ae9c466
Bibek Khatri <55137095+bibekme@users.noreply.github.com>
2020-10-18 06:56:28 +0545

    solved problem1.java

--- programming/problem1.java
+++ programming/problem1.java
@@ -1,3 +1,12 @@
 // "Hello, World!" Challenge
 
 // Write a program in Java to print "Hello, World!"!
+
+package com.company;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
