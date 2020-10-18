05db8c24b12d956396db98d27bf38ff32f079b16
Anjal Binayak <anjal@crystalsoft.com.np>
2020-10-18 16:50:02 +0545

    concatenate two string fro html form

--- programming/problem2.js
+++ programming/problem2.js
@@ -1,3 +1,8 @@
 // "String Concatenation!"
 
 // Write a JavaScript code to concatenate two strings from HTML form, and display the results in the console!
+
+let input1 = document.querySelector("#input1");
+let input2 = document.querySelector("#input2");
+
+console.log(`${input1.value} ${input2.value}`);
