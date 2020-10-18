586e2bd06b0cb2872c36930fa058e23c62111a13
roosaan <khatriroshan54@gmail.com>
2020-10-18 05:58:15 +0545

    added problem3.js

---
+++ programming/problem3.js
@@ -0,0 +1,8 @@
+/*   Addition challange !!
+Hello there !!
+ Can you write a code in javascript that it takes two integers
+ from user as a input and add those and show it in browser !!
+
+The first person to do so in the correct way and create a PR for it would be eligible for his/her PR to be merged.	
+
+*/
