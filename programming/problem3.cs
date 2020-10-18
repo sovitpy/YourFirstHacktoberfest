2e95f45eb1b5fcf68824124a7340e0d4875d9a19
Anjal Binayak <anjal@crystalsoft.com.np>
2020-10-18 16:57:39 +0545

    'string from user input to hello.txt

--- programming/problem3.cs
+++ programming/problem3.cs
@@ -1,3 +1,14 @@
 // "File Writing!"
 
 // Write a program in C# to write the string from user input to "hello.txt"!
+
+
+string folder = @"C:\Temp\";  
+// Filename  
+string fileName = "hello.txt";  
+
+string fullPath = folder + fileName;  
+ 
+string input =  Console.ReadLine();
+ 
+File.WriteAllLines(fullPath, input);  
