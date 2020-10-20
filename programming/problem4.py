1f413d5bbab0f52a9563a1e221ab7698adf12ae5
Aakanksha Agrawal <aakankshaagr2018@gmail.com>
2020-10-21 01:55:48 +0530

    code wasn't written as per instructions so I added a new code

--- programming/problem4.py
+++ programming/problem4.py
@@ -1,6 +1,16 @@
 # Write a program in Python to allow users to enter an arithmetic expression and display the result!
-
-
-x = 4
-y = 10.2
-total = x + y 
+a=int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+op = input("Enter operator : ")
+if(op =='+'):
+    print("The result is :",a+b)
+elif(op =='-'):
+    print("The result is :", a-b)
+elif(op == '*'):
+    print("The result is :", a*b)
+elif(op == '/' and b!=0):
+    print("The result is :", a/b)
+elif(op == '%'):
+    print("The result is :", a%b)
+else:
+    print("Invalid operator!!!!")
